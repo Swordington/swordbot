21875befbf3f2dd23f64c457978b3d58546039ec
The1Sword <quinn@bigbrother.group>
2021-02-15 09:53:27 -0600

    feat(fun): Say Command

---
+++ commands/say.js
@@ -0,0 +1,20 @@
+exports.run = async (client, message, args, level) => {
+    if (args[0] === undefined) return message.channel.send('A message is required')
+    const msg = args.join(' ')
+    message.channel.send(msg)
+    message.delete() // TODO: This should be made optional later on
+  }
+  
+  exports.conf = {
+    aliases: [],
+    permLevel: 0
+  }
+  
+  exports.help = {
+    name: 'say',
+    category: 'Fun',
+    description: 'Say something as the bot',
+    usage: 'say <message>',
+    example: 'say I love Swordbot!'
+  }
+  
